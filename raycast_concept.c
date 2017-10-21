5a8ab88c6123f0bf0069bb2014c86e25993f0073
Sam North <snorth@e1z3r5p6.42.us.org>
2017-10-21 00:29:48 -0700

    textures and sound done, need to norm

--- raycast_concept.c
+++ raycast_concept.c
@@ -6,26 +6,26 @@
 
 # define BITS_PER_PIXEL 32
 # define ENDIAN 1
-# define WIN_W 1000
-# define WIN_H 600
+# define WIN_W 1280
+# define WIN_H 720
 
 # define SCALE 262144
-# define FOV (M_PI / 3)
-# define WALL_HEIGHT (SCALE * 700)
+# define FOV (M_PI / 4)
+# define WALL_HEIGHT (SCALE * WIN_H)
 # define ANGLE_SHIFT (FOV / WIN_W)
-# define ANGLE_START (3 * M_PI / 2)
 
-# define NTEX 1
-# define TEX_RES 64
+# define NTEX 11
+# define SKY_RES 5760
+# define TEX_RES 128
 # define RENDER_SCALE (SCALE / TEX_RES)
 
-# define MOVE_SPEED (5 * (RENDER_SCALE))
+# define MOVE_SPEED (10 * (RENDER_SCALE))
 # define TURN_SPEED (M_PI / 128)
 # define MOUSE_SENS (4 * M_PI / WIN_W)
-# define COLLISION 5
+# define COLLISION 10
 
-# define mapWidth 24
-# define mapHeight 24
+# define MAP_WIDTH 24
+# define MAP_HEIGHT 24
 
 # define KEY_UP 126
 # define KEY_DN 125
@@ -46,33 +46,35 @@
 # define KEY_C 8
 # define KEY_ESC 53
 # define KEY_SP 49
+# define KEY_LSHIFT 257
+# define KEY_M 46
 
-int g_worldMap[mapWidth][mapHeight]=
+int worldMap[MAP_WIDTH][MAP_HEIGHT]=
 {
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1},
-  {1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-  {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+  {1,2,1,2,1,2,1,2,1,2,1,2,1,3,1,2,1,2,1,2,1,3,1,2},
+  {2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+  {2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {3,0,0,0,0,0,4,4,4,4,4,0,0,0,0,5,0,5,0,5,0,0,0,2},
+  {2,0,0,0,0,0,4,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,4,0,0,0,4,0,0,0,0,5,0,0,0,5,0,0,0,3},
+  {2,0,0,0,0,0,4,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,4,4,0,4,4,0,0,0,0,5,0,5,0,5,0,0,0,2},
+  {2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+  {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+  {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+  {2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+  {2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+  {2,5,0,5,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,4,0,0,0,0,6,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+  {2,4,0,5,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,5,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+  {2,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+  {1,5,4,5,4,5,4,5,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+  {1,1,2,1,3,1,2,1,2,1,2,1,3,1,2,1,1,1,2,1,2,1,2,1}
 };
 
 typedef struct	s_mlx
@@ -81,7 +83,6 @@ typedef struct	s_mlx
 	void		*win;
 	void		*img;
 	int			*data;
-
 }				t_mlx;
 
 typedef struct	s_ray
@@ -90,6 +91,7 @@ typedef struct	s_ray
 	int			off;
 	int			tex;
 	int			slice;
+	double		depth;
 }				t_ray;
 
 typedef struct	s_xy
@@ -112,7 +114,40 @@ typedef struct	s_key
 	int			d : 1;
 	int			rt : 1;
 	int			lf : 1;
+	int			shift : 1;
+	int			mute : 1;
+	int			mouse;
 }				t_key;
+
+typedef struct	s_text
+{
+	void		*ptr;
+	int			*img;
+	int			e;
+	int			bpp;
+	int			h;
+	int			w;
+	int			sl;
+}				t_text;
+
+typedef struct	s_menu
+{
+	void		*ptr[3];
+	int			*img[3];
+	int			w[3];
+	int			h[3];
+	int			e;
+	int			bpp;
+	int			sl;
+}				t_menu;
+
+typedef struct	s_minimap
+{
+	void		*ptr[2];
+	int			*img[2];
+	t_fxy		pos;
+	double		dir;
+}				t_minimap;
 
 typedef struct	s_env
 {
@@ -122,15 +157,17 @@ typedef struct	s_env
 	t_ray		H;
 	t_ray		V;
 	t_key		key;
-	void		*tex_img[NTEX];
-	int			*texture[NTEX];
+	t_minimap	*map;
+	t_text		tex[NTEX];
+	t_menu		*menu;
+	int			music;
 }				t_env;
 
 int		check_grid(int x, int y)
 {
 	x /= SCALE;
 	y /= SCALE;
-	if (x >= mapWidth || y >= mapHeight || x < 0 || y < 0)
+	if (x >= MAP_WIDTH || y >= MAP_HEIGHT || x < 0 || y < 0)
 		return (-1);
 	return (worldMap[y][x]);
 }
@@ -149,11 +186,23 @@ int		release_key(int keycode, t_env *env)
 		env->key.a = 0;
 	if (keycode == KEY_D)
 		env->key.d = 0;
+	if (keycode == KEY_LSHIFT)
+		env->key.shift = 0;
+	if (keycode == KEY_M)
+	{
+		if (env->key.mute)
+			system("killall -STOP -c \"afplay\"");
+		else
+			system("killall -CONT -c \"afplay\"");
+		env->key.mute = env->key.mute ? 0 : 1;
+	}
 	return (0);
 }
 
-int		exit_hook()
+int		exit_hook(t_env *env)
 {
+	if (env->music)
+		system("killall -c \"afplay\"");
 	exit(0);
 	return (0);
 }
@@ -166,6 +215,75 @@ void	clear_image(t_mlx *mlx)
 	while (++i < WIN_W * WIN_H)
 		mlx->data[i] = 0;
 	mlx_clear_window(mlx->mlx, mlx->win);
+}
+
+int		fov_color(int length, int bg)
+{
+	int rgb1[3];
+	int rgb2[3];
+	int c2;
+
+	c2 = 0xb0 / 8;
+	rgb1[0] = 0xff - (length * 0xff / 8);
+	rgb1[1] = 0;
+	rgb1[2] = 0;
+	rgb2[0] = (bg >> 16) * length / 8;
+	rgb2[1] = ((bg >> 8) & 0xff) * length / 8;
+	rgb2[2] = (bg & 0xff) * length / 8;
+	return ((((rgb1[0] + rgb2[0]) & 0xff) << 16) | (((rgb1[1] + rgb2[1])\
+			& 0xff) << 8) | (((rgb1[2] + rgb2[2]) & 0xff)));
+}
+
+void	bresen_angle(t_env *env, int x1, int y1)
+{
+	t_fxy xy2;
+	double a1;
+	double a2;
+	int length;
+
+	length = 0;
+	a2 = env->map->dir - (FOV);
+	while (++length <= 8)
+	{
+		a1 = env->map->dir + (FOV);
+		while (a1 >= a2)
+		{
+			xy2.x = x1 + (length * cos(a1));
+			xy2.y = y1 + (length * sin(a1));
+			if ((int)xy2.x >= 0 && (int)xy2.x < 120 && (int)xy2.y >= 0 &&
+					(int)xy2.y < 120)
+				env->map->img[1][(int)xy2.x + (int)xy2.y * 120] =
+						fov_color(length, env->map->img[1]\
+						[(int)xy2.x + (int)xy2.y * 120]);
+			a1 -= (FOV / 10);
+		}
+	}
+}
+
+void	minimap(t_env *env)
+{
+	int i;
+	int x1;
+	int y1;
+
+	env->map->pos.x = 5 * (double)env->pos.x / SCALE;
+	env->map->pos.y = 5 * (double)env->pos.y / SCALE;
+	env->map->dir = -env->dir.x;
+	i = -1;
+	while (++i < (120 * 120))
+		env->map->img[1][i] = env->map->img[0][i];
+	y1 = -3;
+	while (++y1 <= 2)
+	{
+		x1 = -3;
+		while (++x1 <= 2)
+			if (sqrt(x1 * x1 + y1 * y1) < 2)
+				env->map->img[1][(int)(env->map->pos.x + x1) +
+						(int)(env->map->pos.y + y1) * 120] = 0x00ff00;
+	}
+	bresen_angle(env, (int)env->map->pos.x, (int)env->map->pos.y);
+	mlx_put_image_to_window(env->mlx->mlx, env->mlx->win, env->map->ptr[1], 20,
+			WIN_H - 140);
 }
 
 void	horizontal_ray(t_env *env, double angle)
@@ -181,7 +299,8 @@ void	horizontal_ray(t_env *env, double angle)
 		H.x += ((angle > M_PI) ? -Xi : Xi);
 		H.y += ((angle > M_PI) ? SCALE : -SCALE);
 	}
-	env->H.dist = fabs((env->pos.y - H.y) / sin(angle)) * cos(env->dir.x - angle);
+	env->H.dist = fabs((env->pos.y - H.y) /
+			sin(angle)) * cos(env->dir.x - angle);
 	if (env->H.dist < 0)
 		env->H.dist = INT_MAX;
 	env->H.off = (H.x % SCALE) / (RENDER_SCALE);
@@ -193,54 +312,66 @@ void	vertical_ray(t_env *env, double angle)
 	int		Yi;
 
 	Yi = SCALE * tan(angle);
-	V.x = ((env->pos.x / SCALE) * SCALE) + ((angle > M_PI / 2 && angle < 3 * M_PI / 2) ? -1 : SCALE);
+	V.x = ((env->pos.x / SCALE) * SCALE) +
+			((angle > M_PI / 2 && angle < 3 * M_PI / 2) ? -1 : SCALE);
 	V.y = env->pos.y + ((env->pos.x - V.x) * tan(angle));
 	while (!(env->V.tex = check_grid(V.x, V.y)))
 	{
 		V.x += ((angle > M_PI / 2 && angle < 3 * M_PI / 2) ? -SCALE : SCALE);
 		V.y += ((angle > M_PI / 2 && angle < 3 * M_PI / 2) ? Yi : -Yi);
 	}
-	env->V.dist = fabs((env->pos.x - V.x) / cos(angle)) * cos(env->dir.x - angle);
+	env->V.dist = fabs((env->pos.x - V.x) /
+			cos(angle)) * cos(env->dir.x - angle);
 	if (env->V.dist < 0)
 		env->V.dist = INT_MAX;
 	env->V.off = (V.y % SCALE) / (RENDER_SCALE);
 }
 
-int		dumbcolor(int tex, int vert)
+int		color(int color, double depth)
 {
-	int color;
+	int rgb[3];
+	double drgb[3];
 
-	switch(tex)
-	{
-		case 1:  color = vert == 1 ? 0x800000 : 0xff0000; break; //red
-		case 2:  color = vert == 1 ? 0x008000 : 0x00ff00; break; //green
-		case 3:  color = vert == 1 ? 0x000080 : 0x0000ff; break; //blue
-		case 4:  color = vert == 1 ? 0x808080 : 0xffffff; break; //white
-		default: color = vert == 1 ? 0x808000 : 0xffff00; break; //yellow
-	}
-	return (color);
+	rgb[0] = color >> 16;
+	rgb[1] = (color >> 8) & 0xff;
+	rgb[2] = (color) & 0xff;
+	drgb[0] = (double)rgb[0] * sqrt(depth);
+	drgb[1] = (double)rgb[1] * depth;
+	drgb[2] = (double)rgb[2] * depth * depth;
+	rgb[0] = ((int)drgb[0] & 0xff) << 16;
+	rgb[1] = ((int)drgb[1] & 0xff) << 8;
+	rgb[2] = ((int)drgb[2] & 0xff);
+	return (rgb[0] | rgb[1] | rgb[2]);
 }
 
 void	draw(t_env *env, t_ray ray, int x, double angle)
 {
 	double i;
-	if ((ray.slice = WALL_HEIGHT / ray.dist) > 5000)
-		ray.slice = 5000;
+	if ((ray.slice = (WALL_HEIGHT / ray.dist)) > RENDER_SCALE)
+		ray.slice = RENDER_SCALE;
 	int y = (WIN_H - ray.slice) / 2;
-	int ground = y + ray.slice;
+	int ground = y + (ray.slice * 2);
+	int j = -1;
+	while (++j < y)
+		env->mlx->data[x + (j * WIN_W)] = env->tex[NTEX - 1].img\
+				[(int)(angle * SKY_RES / (M_PI * 2)) + (j * SKY_RES)];
 	i = 0;
+	ray.depth = (ray.slice < WIN_H) ? ((double)ray.slice / WIN_H) : 1;
 	while (y < ground)
 	{
 		if (y >= 0 && y < WIN_H)
-			env->mlx->data[x + (y * WIN_W)] = (ray.tex == 1 ? env->texture[ray.tex - 1][ray.off + ((int)i * TEX_RES)] : dumbcolor(ray.tex, (env->V.dist <= env->H.dist)));
+		{
+			env->mlx->data[x + (y * WIN_W)] = color(env->tex[ray.tex].img\
+					[ray.off + ((int)i * TEX_RES)], ray.depth);
+		}
 		y++;
 		i += (64 / (double)ray.slice);
 	}
 	while (y < WIN_H)
-		env->mlx->data[x + (y++ * WIN_W)] = 0xff00ff;
+		env->mlx->data[x + (y++ * WIN_W)] = color(0x808080, ray.depth);
 }
 
-void	raytrace(t_env *env)
+void	raycast(t_env *env)
 {
 	clear_image(env->mlx);
 	double angle = env->dir.x + (FOV / 2);
@@ -257,6 +388,7 @@ void	raytrace(t_env *env)
 		angle -= ANGLE_SHIFT;
 	}
 	mlx_put_image_to_window(env->mlx->mlx, env->mlx->win, env->mlx->img, 0, 0);
+	minimap(env);
 }
 
 void	init_mlx_pointers(t_env *env, char *s)
@@ -277,28 +409,108 @@ void	init_mlx_pointers(t_env *env, char *s)
 	env->mlx = tmp;
 }
 
+char	*texture_asset(int num)
+{
+	if (num == 0)
+		return ("assets/floor.XPM");
+	if (num == 1)
+		return ("assets/wall_1.XPM");
+	if (num == 2)
+		return ("assets/wall_2_vent.XPM");
+	if (num == 3)
+		return ("assets/wall_3_tech.XPM");
+	if (num == 4)
+		return ("assets/wall_4_interior_a.XPM");
+	if (num == 5)
+		return ("assets/wall_5_interior_b.XPM");
+	if (num == 6)
+		return ("assets/wall_end_north.XPM");
+	if (num == 7)
+		return ("assets/wall_end_south.XPM");
+	if (num == 8)
+		return ("assets/wall_end_east.XPM");
+	if (num == 9)
+		return ("assets/wall_end_west.XPM");
+	if (num == 10)
+		return ("assets/skybox.XPM");
+	return (NULL);
+}
+
+void	init_map(t_env *env)
+{
+	int x;
+	int y;
+	int bpp;
+	int e;
+	int w;
+
+	bpp = BITS_PER_PIXEL;
+	e = ENDIAN;
+	w = 120;
+	env->map = (t_minimap*)malloc(sizeof(t_minimap));
+	env->map->ptr[0] = mlx_new_image(env->mlx->mlx, w, w);
+	env->map->ptr[1] = mlx_new_image(env->mlx->mlx, w, w);
+	env->map->img[0] = (int*)mlx_get_data_addr(env->map->ptr[0], &bpp, &w, &e);
+	env->map->img[1] = (int*)mlx_get_data_addr(env->map->ptr[1], &bpp, &w, &e);
+	w /= 4;
+	y = -1;
+	while (++y < w)
+	{
+		x = -1;
+		while (++x < w)
+			env->map->img[0][x + (y * w)] =  worldMap[y / 5][x / 5] ?
+					0x000000 : 0xb0b0b0;
+	}
+}
+
 void	init_env(t_env *env)
 {
-	int texture_size = TEX_RES;
-	int text_bpp = 4;
-	int text_endian = 0;
-	int text_sl = 0;
+	int i;
+
+	env->music = 1;
 	env->pos.x = (22 * SCALE) + (SCALE / 2);
 	env->pos.y = (12 * SCALE) + (SCALE / 2);
 	env->dir.x = M_PI;
 	env->dir.y = 0;
 	ft_bzero(&env->key, sizeof(t_key));
-	for (int i = 0; i < NTEX; i++)
+	i = -1;
+	while (++i < (NTEX))
 	{
-		env->tex_img[i] = mlx_xpm_file_to_image(env->mlx->mlx, "assets/pearlstone_wall.xpm", &texture_size, &texture_size);
-		env->texture[i] = (int*)mlx_get_data_addr(env->tex_img[i], &text_bpp, &text_sl, &text_endian);
+		env->tex[i].w = (i == NTEX - 1) ? SKY_RES : TEX_RES;
+		env->tex[i].h = (i == NTEX - 1) ? 1200 : TEX_RES;
+		env->tex[i].e = 0;
+		env->tex[i].sl = 0;
+		env->tex[i].bpp = 16;
+		env->tex[i].ptr = mlx_xpm_file_to_image(env->mlx->mlx, texture_asset(i),
+				&env->tex[i].w, &env->tex[i].h);
+		env->tex[i].img = (int*)mlx_get_data_addr(env->tex[i].ptr,
+					&env->tex[i].bpp, &env->tex[i].sl, &env->tex[i].e);
 	}
+	init_map(env);
+}
+
+int		mouse_pos(int x, int y, t_env *env)
+{
+	(void)y;
+	if (x < 0 || x > WIN_W || y < 0 || y > WIN_H)
+	{
+		env->key.mouse = 0;
+		return (0);
+	}
+	if (x >= (3 * WIN_W / 7) && x < (4 * WIN_W / 7))
+		env->key.mouse = 0;
+	else
+		env->key.mouse = (x - (WIN_W / 2));
+	return (0);
 }
 
 int		press_key(int keycode, t_env *env)
 {
 	if (keycode == KEY_ESC)
+	{
+		system("killall -c \"afplay\"");
 		exit(0);
+	}
 	if (keycode == KEY_RT)
 		env->key.rt = 1;
 	if (keycode == KEY_LF)
@@ -311,6 +523,8 @@ int		press_key(int keycode, t_env *env)
 		env->key.a = 1;
 	if (keycode == KEY_D)
 		env->key.d = 1;
+	if (keycode == KEY_LSHIFT)
+		env->key.shift = 1;
 	return (0);
 }
 
@@ -319,14 +533,21 @@ int		hook_func(t_env *env)
 	int movex;
 	int movey;
 
+	if (env->key.mouse)
+	{
+		env->dir.x -= TURN_SPEED * (double)env->key.mouse / WIN_W;
+		while (env->dir.x < 0 || env->dir.x > 2 * M_PI)
+			env->dir.x += (2 * M_PI) * (env->dir.x < 0 ? 1 : -1);
+	}
 	if (env->key.lf || env->key.rt)
 	{
 		env->dir.x += TURN_SPEED * (env->key.rt ? -1 : 1);
 		while (env->dir.x < 0 || env->dir.x > 2 * M_PI)
 			env->dir.x += (2 * M_PI) * (env->dir.x < 0 ? 1 : -1);
+		printf("%lf\n", env->dir.x);
 	}
-	movex = cos(env->dir.x) * MOVE_SPEED;
-	movey = sin(env->dir.x) * MOVE_SPEED;
+	movex = cos(env->dir.x) * MOVE_SPEED * (env->key.shift ? 1.5 : 1);
+	movey = sin(env->dir.x) * MOVE_SPEED * (env->key.shift ? 1.5 : 1);
 	if (env->key.w)
 	{
 		if(!check_grid(env->pos.x + (COLLISION * movex), env->pos.y))
@@ -341,8 +562,8 @@ int		hook_func(t_env *env)
 		if(!check_grid(env->pos.x, env->pos.y + (COLLISION * movey)))
 			env->pos.y += movey;
 	}
-	movex = cos(env->dir.x - (M_PI / 2)) * (MOVE_SPEED / 2);
-	movey = sin(env->dir.x - (M_PI / 2)) * (MOVE_SPEED / 2);
+	movex = cos(env->dir.x - (M_PI / 2)) * (MOVE_SPEED / 2) * (env->key.shift ? 1.5 : 1);
+	movey = sin(env->dir.x - (M_PI / 2)) * (MOVE_SPEED / 2) * (env->key.shift ? 1.5 : 1);
 	if (env->key.a)
 	{
 		if(!check_grid(env->pos.x - (COLLISION * movex), env->pos.y))
@@ -357,20 +578,105 @@ int		hook_func(t_env *env)
 		if(!check_grid(env->pos.x, env->pos.y - (COLLISION * movey)))
 			env->pos.y -= movey;
 	}
-	raytrace(env);
+	raycast(env);
 	return (0);
+}
+
+void	wolf(t_env *env)
+{
+	system("afplay assets/boi_basement.mp3 &");
+	init_env(env);
+	mlx_hook(env->mlx->win, 2, 0, press_key, env);
+	mlx_hook(env->mlx->win, 3, 0, release_key, env);
+	mlx_hook(env->mlx->win, 6, 0, mouse_pos, env);
+	mlx_hook(env->mlx->win, 17, 0, exit_hook, env);
+	mlx_loop_hook(env->mlx->mlx, hook_func, env);
+	mlx_loop(env->mlx->mlx);
+}
+
+int		menu_hook(int mbutton, int x, int y, t_env *env)
+{
+	int i;
+
+	if (mbutton != 1)
+		return (0);
+	if (y >= (WIN_H / 2) + 20 && y <= (WIN_H / 2) + 148)
+	{
+		if (x >= (2 * WIN_W / 3) - 64 && x <= (2 * WIN_W / 3) + 64)
+			exit(0);
+		if (x >= (WIN_W / 3) - 64 && x <= (WIN_W / 3) + 64)
+		{
+			i = -1;
+			while (++i < 3)
+				mlx_destroy_image(env->mlx->mlx, env->menu->ptr[i]);
+			wolf(env);
+		}
+	}
+	return (0);
+}
+
+char	*menu_gfx(int code)
+{
+	if (code == 0)
+		return ("assets/menu_bg.XPM");
+	if (code == 1)
+		return ("assets/start_statue.XPM");
+	if (code == 2)
+		return ("assets/exit_wizard.XPM");
+	return (NULL);
+}
+
+int		menu_exit(int keycode, t_env *env)
+{
+	(void)env;
+	if (keycode == 53)
+		exit(0);
+	return (0);
+}
+
+void	menu_loop(t_env *env, t_menu *menu)
+{
+	mlx_put_image_to_window(env->mlx->mlx, env->mlx->win, menu->ptr[0], 0, 0);
+	mlx_put_image_to_window(env->mlx->mlx, env->mlx->win, menu->ptr[1],
+			(WIN_W / 3) - 64, (WIN_H / 2) + 20);
+	mlx_put_image_to_window(env->mlx->mlx, env->mlx->win, menu->ptr[2],
+			(2 * WIN_W / 3) - 64, (WIN_H / 2) + 20);
+	mlx_string_put(env->mlx->mlx, env->mlx->win, (WIN_W / 3) - 25, (WIN_H / 2),
+			0xffffff, "START");
+	mlx_string_put(env->mlx->mlx, env->mlx->win, (2 * WIN_W / 3) - 20,
+			(WIN_H / 2), 0xffffff, "EXIT");
+	mlx_hook(env->mlx->win, 2, 0, menu_exit, env);
+	mlx_hook(env->mlx->win, 17, 0, exit_hook, env);
+	mlx_mouse_hook(env->mlx->win, menu_hook, env);
+	mlx_loop(env->mlx->mlx);
+}
+
+void	menu(t_env *env)
+{
+	int i;
+	t_menu	menu;
+
+	i = -1;
+	while (++i < 3)
+	{
+		menu.w[i] = (i == 0) ? 1280 : 128;
+		menu.h[i] = (i == 0) ? 720 : 128;
+		menu.e = 0;
+		menu.bpp = 16;
+		menu.sl = 0;
+		menu.ptr[i] = mlx_xpm_file_to_image(env->mlx->mlx,
+				menu_gfx(i), &menu.w[i], &menu.h[i]);
+		menu.img[i] = (int*)mlx_get_data_addr(menu.ptr[i], &menu.bpp, &menu.sl, &menu.e);
+	}
+	env->music = 0;
+	env->menu = &menu;
+	menu_loop(env, &menu);
 }
 
 int		main()
 {
 	t_env	env;
 
-	init_mlx_pointers(&env, "hello world");
-	init_env(&env);
-	raytrace(&env);
-	mlx_hook(env.mlx->win, 2, 0, press_key, &env);
-	mlx_hook(env.mlx->win, 3, 0, release_key, &env);
-	mlx_hook(env.mlx->win, 17, 0, exit_hook, &env);
-	mlx_loop_hook(env.mlx->mlx, hook_func, &env);
-	mlx_loop(env.mlx->mlx);
+	init_mlx_pointers(&env, "Wolf 3D");
+	menu(&env);
 }
